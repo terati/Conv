9fc8047ce8daf7fb8e888d1e4ed85e54101cd4ed
Tim <wongtimothy13@gmail.com>
2021-07-30 11:02:18 -0500

    update

--- convolution.js
+++ convolution.js
@@ -1,27 +1,3 @@
-// var counter = 0;
-// function init()
-// {
-//     canvas=document.getElementById("gameCanvas")
-//     ctx = canvas.getContext('2d');
-
-//     setInterval(draw,1000);
-
-// }
-// init();
-
-
-// function draw()
-// {
-//     ctx.clearRect(0,0, canvas.width, canvas.height);
-//     ctx.beginPath();
-//     ctx.arc(100, 100, 50, 0, counter); //2 * Math.PI);
-//     ctx.stroke();
-//     if (counter > 2* Math.PI){
-//         counter = 0;
-//     } else {
-//         counter += 0.5;
-//     }
-// }
 
 var canvas=document.getElementById("gameCanvas");
 var ctxt = canvas.getContext('2d');
